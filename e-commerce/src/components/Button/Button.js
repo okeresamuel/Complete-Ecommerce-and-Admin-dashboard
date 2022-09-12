1e657e0f5e484556ab493bc682a85873bb441981
jab2ez <you@example.com>
2022-09-12 16:38:53 -0500

    feature/5 working on button component

---
+++ e-commerce/src/components/Button/Button.js
@@ -0,0 +1,10 @@
+import "./Button.css";
+
+const Button = () => {
+    return <div class="button">
+        Some Text {/*This will be some user imputted text */}
+        <img src="https://www.freeiconspng.com/uploads/shopping-cart-icon-2.png"  alt="Cart Icon" width="20" />
+    </div>
+}
+
+export default Button;
--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import './Header.css'
+import Button from '../Button/Button.js'
 
 const Header = () => {
     return (<div id='header' className='greyBorder'>
         Header
+        <Button />
     </div>)
 }
 
