25de11d489d81c1f59f1e06bca2c990386eb9cb2
okeresamuel <okeresamuel360@gmail.com>
2022-09-13 12:43:53 +0100

    Create Search bar

--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -1,12 +1,14 @@
+import SearchIcon from '@mui/icons-material/Search';
+import GrainIcon from '@mui/icons-material/Grain';
 import './Header.css'
-import Button from '@mui/material/Button';
-
-
 
 const Header = () => {
     return (<div id='header'>
-      <input className="search__box"  type="search"></input>
-      <Button>gsa</Button>
+      <div className="search__box">
+      <SearchIcon className='search__Icon' />
+      <input className='search__Input' type="search" placeholder='Search among 100+ products'></input>
+      <GrainIcon className='Grain__Icon' />
+      </div>
     </div>)
 }
 
