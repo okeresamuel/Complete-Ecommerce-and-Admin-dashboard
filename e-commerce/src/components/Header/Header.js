a1cc8354124accfd6602d2b0ac3625bd7e194efe
okeresamuel <okeresamuel360@gmail.com>
2022-09-13 11:40:50 +0100

    add searchbar

--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -1,8 +1,12 @@
 import './Header.css'
+import Button from '@mui/material/Button';
+
+
 
 const Header = () => {
-    return (<div id='header' className='greyBorder'>
-        Header
+    return (<div id='header'>
+      <input className="search__box"  type="search"></input>
+      <Button>gsa</Button>
     </div>)
 }
 
