194bac423eba98e7138fd65f76fc4b0e8dbe3a25
jab2ez <you@example.com>
2022-09-13 16:07:16 -0500

    feature/5 finished making button

--- e-commerce/src/components/Button/Button.js
+++ e-commerce/src/components/Button/Button.js
@@ -1,9 +1,19 @@
 import "./Button.css";
 
-const Button = () => {
-    return <div class="button">
-        Some Text {/*This will be some user imputted text */}
-        <img src="https://www.freeiconspng.com/uploads/shopping-cart-icon-2.png"  alt="Cart Icon" width="20" />
+const ButtonImage = (props) => {
+    const { imageSource, imageWidth, imageLeftPadding } = props
+
+    return (
+        <img class="buttonImage" src={imageSource || ""}  alt={imageSource ? "Button Image" : ""} style={{width:imageWidth || "20px",  paddingLeft:imageLeftPadding || ""}}/>
+    )
+}
+
+const Button = (props) => {
+    const { text, width, backgroundColor, textColor, fontWeight, fontSize, imageSource, imageWidth, imageLeftPadding } = props
+        
+    return <div class="button" style={{background:backgroundColor || "#fffeff", borderRadius:"8px", color:textColor || "#34237b", maxWidth:width || "90px", fontWeight: fontWeight || "600", fontSize: fontSize || "14px"}}>
+        {text || ""} {/*This will be some user imputted text */}
+        <ButtonImage imageSource={imageSource} imageWidth={imageWidth} imageLeftPadding={imageLeftPadding} />
     </div>
 }
 
--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import Button from '../Button/Button.js'
 const Header = () => {
     return (<div id='header' className='greyBorder'>
         Header
-        <Button />
+        <Button text="Button" width="100px" backgroundColor="#d3cede" textColor="#2a01d3" fontWeight="600" imageSource="https://www.freeiconspng.com/uploads/shopping-cart-icon-2.png" imageWidth="20px" imageLeftPadding="14px" fontSize="15px" />
     </div>)
 }
 
