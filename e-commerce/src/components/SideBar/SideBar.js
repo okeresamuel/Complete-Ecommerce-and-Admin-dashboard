b567fd6e8efc719047659cf66d67ebfdb23f2a4c
okeresamuel <okeresamuel360@gmail.com>
2022-09-14 13:55:29 +0100

    Create category section

--- e-commerce/src/components/SideBar/SideBar.js
+++ e-commerce/src/components/SideBar/SideBar.js
@@ -2,7 +2,15 @@ import './SideBar.css'
 
 const SideBar = () => {
     return (<div id='sideBar' className='greyBorder'>
-       <h4>Category</h4>
+       <h4 className='Category__text'>Category</h4>
+       <div className='link__container'>
+       <a href="#Closed"  className="link">T-Shirt</a>
+       <a href="#Closed"  className="link">Sweatshirt</a>
+       <a href="#Closed"  className="link">Dress</a>
+       <a href="#Closed"  className="link">Pants and Skirt</a>
+       <a href="#Closed"  className="link">Swinsuit</a>
+       <a href="#Closed"  className="link">Stuff and Accessories</a>
+       </div>
     </div>)
 }
 
