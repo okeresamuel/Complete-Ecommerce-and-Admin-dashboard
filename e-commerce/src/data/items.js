f9f1869b5ea4711eb42ae834fe68eef8d1b02a32
tgoggans <goggans_t@yahoo.com>
2022-09-15 20:53:46 -0400

    updated

---
+++ e-commerce/src/data/items.js
@@ -0,0 +1,67 @@
+const items = [
+    {
+        id: 1,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+    {
+        id: 2,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+    {
+        id: 3,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+    {
+        id: 4,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+    {
+        id: 5,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+    {
+        id: 6,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+    {
+        id: 7,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+    {
+      
+        id: 8,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+    {
+        id: 9,
+        name: "Product",
+        price: 0,
+        image: ""
+
+    },
+
+]
