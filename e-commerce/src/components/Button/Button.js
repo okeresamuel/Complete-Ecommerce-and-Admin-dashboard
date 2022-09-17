25795cefe24f16b900ac1c1aa5ae39ac51c18f55
okeresamuel <okeresamuel360@gmail.com>
2022-09-17 21:04:33 +0100

    arranged btns

--- e-commerce/src/components/Button/Button.js
+++ e-commerce/src/components/Button/Button.js
@@ -56,7 +56,7 @@ const Button = (props) => {
         }
     };
 
-    return <div class="button" style={{ background: backgroundColor || "#fffeff", borderRadius: "8px", color: textColor || "#34237b", maxWidth: width || "90px", height: height || "20px", fontWeight: fontWeight || "600", fontSize: fontSize || "14px" }}>
+    return <div class="button" style={{ background: backgroundColor || "#fffeff", borderRadius: "65px", color: textColor || "#34237b", maxWidth: width || "90px", height: height || "20px", fontWeight: fontWeight || "600", fontSize: fontSize || "14px" }}>
         {text || ""} {/*This will be some user imputted text */}
         {iconSelection(iconPicture)}
     </div>
--- e-commerce/src/components/Main/Main.js
+++ e-commerce/src/components/Main/Main.js
@@ -1,14 +1,17 @@
 import './Main.css'
 import Products from '../products/Products';
+import Button from '../Button/Button.js';
 
 const Main = () => {
     return (
         <>
-            <div id='main' className='greyBorder'>Main
+            <div id='main' className='greyBorder'>
+            <>
+            <Button text="Hello"  width="20px" height="20px" backgroundColor="blue" textColor="white" fontWeight="bold" fontSize="10px" imageSource="https://cdn-icons-png.flaticon.com/128/5349/5349022.png" imageWidth="60px" imageLeftPadding="14px" />
             <Products></Products>
+            </>
             </div>
             
         </>)
 }
-
 export default Main;
