c99e8b744aaf37f9046b54d6c67df5e642a2b0b9
jab2ez <you@example.com>
2022-09-21 14:29:16 -0500

    feature/33 added and styled the page buttons at the bottom of the page

--- e-commerce/src/components/PageButtons/PageButton.js
+++ e-commerce/src/components/PageButtons/PageButton.js
@@ -1,16 +1,26 @@
 import "./PageButton.css"
 import Button from "../Button/Button"
-import { useState } from "react";
+// import { useState } from "react";
+
+const pageNumberIcons = (number) => {
+        
+    return <Button extraClass="pageButton" text={number} textColor="#a29fa3" width="80px" height="20px" padding="20px" borderRadius="5px" />
+}
 
 const PageButton = () => {
-    const pageButtons = [1, 2, 3 /* currentPage */, 4, 5, "...", 20 /* totalPages */];
+    const pageButtons = [1, 2, 3 /* currentPage */, 4, 5, "..." /* over seven pages ? add "..." at index 5 put total pages at the end */, 20 /* totalPages */];
+    
     // const [pageButtons, setPageButtons] = useState([])
     
     return  (
         <div id="pageButtonsSection">
-            <p class="pageButton">Previous</p>
-            <Button  text="1" width="20px" height="20px" padding="0px" />
-            <p class="pageButton">Next</p>
+            <Button text="Previous" textColor="#adaaad" width="60px" height="20px" padding="0px" />
+            {
+            pageButtons.map((number) => {
+                return pageNumberIcons(number)
+            })
+            }
+            <Button text="Next" textColor="#adaaad" width="80px" height="20px" padding="0px" />
         </div>
     )
 }
