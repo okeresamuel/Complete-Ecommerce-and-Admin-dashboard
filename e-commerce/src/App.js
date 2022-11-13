d2f4fe7d70fdbb9087fb0a655e0617a0b19027f8
tgoggans <goggans_t@yahoo.com>
2022-11-13 10:23:10 -0500

    updated 11-13

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -2,19 +2,23 @@ import Header from './components/Header/Header';
 import Logo from './components/Logo/Logo';
 import SideBar from './components/SideBar/SideBar';
 import Main from './components/Main/Main';
-
+import Cart from './components/Cart/Cart.js'
+import { createContext, useState } from 'react';
 import './App.css';
+import CartProvider from './context/CartContext';
+
 
 
 function App() {
+
   return (
-    <div id="app">
+    <CartProvider>
       <Logo />
       <Header />
       <SideBar />
       <Main />
-    </div>
-);
+    </CartProvider>
+  );
 }
 
 export default App;
