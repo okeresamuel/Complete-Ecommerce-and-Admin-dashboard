04700a408eb0aaea363c563a6be14aeec5bceb1e
okeresamuel <okeresamuel360@gmail.com>
2022-11-15 17:09:20 +0100

    updated page item display and favourite icon

--- e-commerce/src/components/products/Products.js
+++ e-commerce/src/components/products/Products.js
@@ -1,6 +1,5 @@
 
-import { Card, CardMedia, CardContent, Typography, CardActions, Grid, FormRow, Box, Button } from "@mui/material";
-import { margin } from "@mui/system";
+import { Button } from "@mui/material";
 import { items } from "../../data/items";
 import "./products.css"
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
@@ -10,38 +9,31 @@ import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 const Products = () => {
     return (
-        <div className="Products__display">
-            <Grid container xs={12} columnSpacing={{ xs: 5, md: 2 }}>
-                {items.map((item) => (
-                    <Grid item xs={3.5} key={item.id} columnSpacing={1} >
-                        <Card sx={{ maxWidth: 350 }} >
-                            {item.tag &&
-                                <div className="Promo_Iconz">
-                                    <Button style={{ backgroundColor: 'hsl(250deg 57% 97%)', opacity: '85%', color: 'blue' }}>{item.tag}</Button>
-                                </div>}
-                            <CardMedia
-                                component="img"
-                                height="350"
-                                image={item.image}
-                                alt="shirt" />
-                            <CardContent>
-                                <Typography gutterBottom variant="h5" component="div">
-                                    {item.name}
-                                </Typography>
-                                <Typography variant="body2" color="text.secondary">
-                                    {`Price: $${item.price}`}
-                                </Typography>
-                                <div className="Shopping_Button">
-                                    <ShoppingCartIcon />
-                                </div>
-                            </CardContent>
-                        </Card>
-                        <div className="Favorite_Icon">
-                            <FavoriteBorderIcon className='love__icon' />
-                        </div>
-                    </Grid>
+        <div className="Products__display">  
+            {items.map((item) => (
+                 <div className="shirt__container"> 
+                   
+                  <div className="Favorite_Icon">
+                  <FavoriteBorderIcon />
+                  </div>
+
+                     {item.tag &&
+                       <div className="Promo_Iconz">
+                       <Button style={{ backgroundColor: 'hsl(250deg 57% 97%)', opacity: '85%', color: 'blue' }}>{item.tag}</Button>
+                     </div>}    
+                 
+                     <img className="shirt__img" src={item.image} alt="shirts" />              
+                     
+                      <div className="sub__shirtcontainer">
+                      <h3>{item.name}</h3>
+                      <span className="priceand__cartbtnContainer">
+                      {`Price: $${item.price}`}
+                      <ShoppingCartIcon className="Shopping_Button" />
+                      </span>
+                     </div>
+
+                 </div>
                 ))}
-            </Grid>
         </div>
     )
 
