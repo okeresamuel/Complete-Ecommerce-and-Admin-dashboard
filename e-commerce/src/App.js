3c4fe62dd2d236f42e8915607af6e017f8394341
okeresamuel <okeresamuel360@gmail.com>
2022-11-18 20:43:08 +0100

    added navigation to cart

--- e-commerce/src/App.js
+++ e-commerce/src/App.js
@@ -2,7 +2,8 @@ import Header from './components/Header/Header';
 import Logo from './components/Logo/Logo';
 import SideBar from './components/SideBar/SideBar';
 import Main from './components/Main/Main';
-import Cart from './components/Cart/Cart.js'
+import {BrowserRouter as Router, Routes, Route,} from "react-router-dom"
+import {Cart, Errorpage} from "./pages/pageexporter"
 import { createContext, useState } from 'react';
 import './App.css';
 import CartProvider from './context/CartContext';
@@ -12,11 +13,19 @@ import CartProvider from './context/CartContext';
 function App() {
   return (
     <div id="app">
-      <Logo />
-      <Header />
-      <SideBar />
-      <Main />
-    </div>
+    <Router>
+    <Logo /> 
+    <Header />
+    <SideBar />
+    <Routes>
+      <>
+      <Route path="/"  element={<Main />}/>
+      <Route path="/cart"  element={<Cart/>}/>
+      <Route path="*"  element={<Errorpage />}/>
+      </>
+    </Routes>
+    </Router>
+    </div>    
 );
 }
 
--- e-commerce/src/components/Header/Header.js
+++ e-commerce/src/components/Header/Header.js
@@ -4,6 +4,7 @@ import LanguageIcon from '@mui/icons-material/Language';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import ExitIcon from '@mui/icons-material/HighlightOff';
+import {Link} from "react-router-dom"
 import './Header.css';
 import { useContext, useRef } from 'react';
 import { useCart,useCartUpdate } from '../../context/CartContext';
@@ -15,6 +16,7 @@ const Header = () => {
   //update the global state of the cart
   const cartUpdate = useCartUpdate()
 
+  
     const languageConatainer__ref = useRef()
     const languageConatainerExiticon__ref = useRef()
     const  ShowChart = () => {
@@ -26,7 +28,7 @@ const Header = () => {
       languageConatainerExiticon__ref.current.style.visibility = "hidden"
     }
     
-    return (<div id='header'>
+    return (<div id='header' className='greyBorder'>
       <div className="search__box">
       <SearchIcon className='search__Icon' />
       <input className='search__Input' type="search" placeholder='Search among 100+ products'></input>
@@ -44,7 +46,7 @@ const Header = () => {
         </select> 
         <LanguageIcon  className="Language__Icon" />         
         <a href="#closed" className="Nav__btn1">Wishlist <FavoriteBorderIcon className='love__icon'/></a>
-        <a href="#closed" className="Nav__btn2" onClick={cartUpdate}>Your Cart <ShoppingCartIcon className='Shopping__icon'/></a>
+        <Link to="/cart" className="Nav__btn2" onClick={cartUpdate}>Your Cart <ShoppingCartIcon className='Shopping__icon'/></Link>
         </div> 
       
 
